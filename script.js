4b4568e41e48e627d1f52c5c6a8a6cfb3020e6a4
Admin <purohitparthiv@gmail.com>
2024-04-13 22:53:12 +0530

    initial commit

--- script.js
+++ script.js
@@ -8,20 +8,41 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  const employees = [...arr];
+  employees.map((employee) => {
+    if (employee.profession === "developer") console.log(employee.name);
+  });
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  const employees = [...arr];
+  employees.forEach((employee) => {
+    if (employee.profession === "developer") console.log(employee.name);
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  const newEmployee = { id: 4, name: "susan", age: "20", profession: "intern" };
+  arr.push(newEmployee);
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  const employees = [...arr];
+  arr = employees.filter((employee) => employee.profession !== "admin");
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const states = [
+    { id: 21, state: "Delhi", country: "india" },
+    { id: 22, fruit: "mango", season: "summer" },
+    { id: 23, hotel: "holiday", city: "Bijnor" },
+  ];
+  const concatedArr = arr.concat(states);
+  console.log(concatedArr);
 }
